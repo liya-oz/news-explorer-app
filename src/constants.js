517cf0fb197a9eb5230e6eec0311bd2fc1b59fad
Liya Oz <143719507+liya-oz@users.noreply.github.com>
2024-11-13 16:51:46 +0100

    constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // constants.js
 export const API_BASE_URL = 'https://content.guardianapis.com';
-export const API_KEY = '6cff0ae8-a6e5-4597-a67e-f444a36c2350'; // Add your Guardian API key here
+export const API_KEY = '6cff0ae8-a6e5-4597-a67e-f444a36c2350';
