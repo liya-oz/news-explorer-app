56bb06dcb2a1ca0243f2c8f6227faafd17b37ac9
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-05-19 01:34:48 +0200

    Set correct PROXY_BASE_URL for production

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,5 @@
-export const PROXY_BASE_URL = 'http://localhost:5000/proxy';
+const isLocalhost = window.location.hostname === 'localhost';
+
+export const PROXY_BASE_URL = isLocalhost
+  ? 'http://localhost:5000/proxy'
+  : 'https://news-explorer-app.onrender.com/proxy';
